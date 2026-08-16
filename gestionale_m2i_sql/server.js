a450a98b12eced2886ca1b597109bdb7320ad124
M2I Admin <admin@m2i.it>
2026-08-16 23:25:14 +0200

    Fix PORT env var for Render

--- gestionale_m2i_sql/server.js
+++ gestionale_m2i_sql/server.js
@@ -8,7 +8,7 @@ const excelGenerator = require('./excel_generator');
 const { knex } = require('./db');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
